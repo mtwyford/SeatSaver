3daf36b250538e63ed76bbfabb131d1943a47ffe
Marcus T <mtwyford@users.noreply.github.com>
2022-03-09 23:46:25 -0500

    Create Main.java

---
+++ StarterCode/Main.java
@@ -0,0 +1,18 @@
+//Main.java - a new approach to the tester file, this file supports the Replit.com file structure where the main tester class is Main.java
+//Don't forget to add you own name and date... 10% of your grade quickly disappears if it slips your mind!
+public class Main {
+  
+  //psvmsa
+  
+  // add 'menu' to walk users through process of loading file, 
+  // adding to an Arrayist of Person 'objects'
+  //then asking users for registration info to add the remaining 10 people
+  // then call the "seating' method (you decide naming rights
+   
+  //this should place people at tables (what data structure will you use?  more ArrayLists?
+  //you can't simply create an array of array lists, but what about an 2D[ ]  of table objects??
+  // Is that possible?
+  
+  
+  
+}
