ab5138498b4e6827d48f324e5bff4bec1f1763e7
Marcus T <mtwyford@users.noreply.github.com>
2022-03-09 23:51:20 -0500

    Create Person.java

---
+++ StarterCode/Person.java
@@ -0,0 +1,15 @@
+public class Person {
+  
+ /*fields or variables for each person
+ * //personID, Name (First, Last), CompanyID, table?
+ * //(would it be helpful, once you've completed the seating arrangement, to record what table each person is at?)
+ */ 
+  
+  //Constructor - build a constructor to 'create' a person, by passing to the constructor, their ID, Name (F,L), and Company
+  
+  //Are any Methods Needed?
+  
+  
+  //Would a toString method be helpful?
+  
+}
